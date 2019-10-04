7aeb6fd4652917230c7143ed06d10bc126a7afde
sadfgh12 <48231918+sadfgh12@users.noreply.github.com>
2019-10-05 01:14:28 +0900

    Add files via upload

---
+++ src/chatting/src/chatting/Main.java
@@ -0,0 +1,16 @@
+package chatting;
+import javax.swing.*;
+
+public class Main {
+	Openwindow openwindow;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main main = new Main();
+		main.openwindow = new Openwindow();
+		main.openwindow.mainSetting(main);
+	}
+
+}
+
+
+
---
+++ src/chatting/src/chatting/Menu.java
@@ -0,0 +1,108 @@
+package chatting;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Menu extends JFrame{
+	private JRadioButtonMenuItem colorItems[],fonts[];
+	private JCheckBoxMenuItem styleItems[];
+	private ButtonGroup fontGroup, colorGroup;
+	private Openwindow openwindow;
+	private JFrame Menuframe;
+	private static ImageIcon ic1;
+	private static JLabel image1;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Menu();
+	}
+	
+	public Menu(){
+		Menuframe = new JFrame("수지");
+		ic1 = new ImageIcon("./image2.png");
+		image1 = new JLabel(ic1);
+		JMenuBar bar = new JMenuBar();
+		setJMenuBar(bar);
+		
+		JMenu ChatMenu = new JMenu("채팅(S)");
+		ChatMenu.setToolTipText("채팅 메뉴");
+		ChatMenu.setMnemonic('S');
+		
+		JMenuItem newItem = new JMenuItem("새로운 채팅(N)");
+		newItem.setMnemonic('N');
+		ChatMenu.add(newItem);
+		
+		JMenuItem openItem = new JMenuItem("오픈 채팅(O)");
+		newItem.setMnemonic('O');
+		ChatMenu.add(openItem);
+		
+		JMenuItem exitItem = new JMenuItem("닫기(X)");
+		exitItem.setMnemonic('X');
+		ChatMenu.add(exitItem);
+		exitItem.addActionListener(new ActionListener() {
+	        	@Override
+	        	public void actionPerformed(ActionEvent e) {
+	        		System.exit(0);
+	        	}
+	        });
+		
+		bar.add(ChatMenu);
+		
+		JMenu formatMenu = new JMenu("편집(E)");
+		formatMenu.setToolTipText("편집 메뉴");
+		formatMenu.setMnemonic('E');
+		
+		String colors[] = {"Dark","Light","Red","Green","Blue"};
+		JMenu colorMenu = new JMenu("색상(C)");
+		colorMenu.setMnemonic('C');
+		colorItems = new JRadioButtonMenuItem[colors.length];
+		colorGroup = new ButtonGroup();
+		
+		for (int i=0;i<colors.length;i++)
+		{
+			colorItems[i] = new JRadioButtonMenuItem(colors[i]);
+			colorMenu.add(colorItems[i]);
+		}
+		
+		colorItems[0].setSelected(true);
+		formatMenu.add(colorMenu);
+		//formatMenu.addSeparator();
+		
+		String fontNames[]= {"굴림","바탕"};
+		JMenu fontMenu = new JMenu("글꼴(T)");
+		fonts = new JRadioButtonMenuItem[fontNames.length];
+        fontGroup = new ButtonGroup();
+        
+        for ( int i = 0; i < fonts.length; i++ ) 
+        {
+            fonts[ i ] = new JRadioButtonMenuItem( fontNames[ i ] );
+            fontMenu.add( fonts[ i ] );
+            fontGroup.add( fonts[ i ] );
+        }
+        fonts[0].setSelected(true);
+        //fontMenu.addSeparator();
+        
+        bar.add(formatMenu);
+        
+        JMenu helpMenu = new JMenu( "도움말(H)" );
+        helpMenu.setToolTipText("도움말 메뉴입니다");
+        helpMenu.setMnemonic( 'H' );
+        JMenuItem helpItem = new JMenuItem( "도움말항목(L)" );
+        helpItem.setMnemonic( 'L' );
+        helpMenu.add(helpItem);
+        bar.add( helpMenu );
+        add(image1); 
+		setBounds(200,300,640,360);
+		setVisible(true);
+		setResizable(false);
+		//frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setSize( 400, 200 );
+        //setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public void menuSetting(Openwindow openwindow) {
+		this.openwindow=openwindow;
+	}
+
+}
